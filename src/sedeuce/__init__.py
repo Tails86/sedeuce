1198234c76f66ef32531c9a68ddf99d2f1a3227d
James Smith <jmsmith86@gmail.com>
2023-10-01 13:29:59 -0600

    Import SedArgParser; added release workflow; bumped version

--- src/sedeuce/__init__.py
+++ src/sedeuce/__init__.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from .sed import Sed
+from .sed import Sed, SedArgParser
 
 from .conditions import StaticSedCondition, RangeSedCondition, RegexSedCondition
 
