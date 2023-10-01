4cad779c2992bed9c0e87ecda0ce860f0eb0a833
James Smith <jmsmith86@gmail.com>
2023-10-01 11:22:17 -0600

    Added separate Sed parsing object to create Sed object from CLI args

--- src/sedeuce/sed.py
+++ src/sedeuce/sed.py
@@ -166,91 +166,94 @@ class Sed:
                     fp.write(tmp_file.getvalue())
         return 0
 
-def parse_args(cliargs:List[str]):
-    ''' Parses all arguments from command line '''
-    parser = argparse.ArgumentParser(
-        prog=PACKAGE_NAME,
-        description='A sed clone in Python with both CLI and library interfaces'
-    )
+class SedArgParser:
+    def __init__(self, cliargs:List[str]) -> None:
+        self.args = __class__._parse_args(cliargs)
 
-    parser.add_argument('script', type=str, nargs='?', default=None,
-                        help='script, only if no other script defined below')
-    parser.add_argument('input_file', metavar='input-file', type=str, nargs='*', default=[],
-                        help='Input file(s) to parse')
+    @staticmethod
+    def _parse_args(cliargs:List[str]):
+        ''' Parses all arguments from command line '''
+        parser = argparse.ArgumentParser(
+            prog=PACKAGE_NAME,
+            description='A sed clone in Python with both CLI and library interfaces'
+        )
 
-    parser.add_argument('-n', '--quiet', '--silent', dest='quiet', action='store_true',
-                        help='suppress automatic printing of pattern space')
-    # No debug data
-    parser.add_argument('--debug', action='store_true', help='annotate program execution')
-    parser.add_argument('-e', '--expression', metavar='script', type=str, default=[], action='append',
-                        help='add the script to the commands to be executed')
-    parser.add_argument('-f', '--file', metavar='script-file', type=str, default=[], action='append',
-                        help='add the contents of script-file to the commands to be executed')
-    parser.add_argument('--follow-symlinks', action='store_true',
-                        help='follow symlinks when processing in place')
-    parser.add_argument('-i', '--in-place', metavar='SUFFIX', nargs='?', type=str, default=None,
-                        const=True,
-                        help='edit files in place (makes backup if SUFFIX supplied)')
-    parser.add_argument('-l', '--line-length', metavar='N', type=int, default=70,
-                        help='specify the desired line-wrap length for the `l\' command')
-    # This option currently has no effect
-    parser.add_argument('--posix', action='store_true', help='disable all extensions.')
-    parser.add_argument('-E', '-r', '--regexp-extended', action='store_true',
-                        help='use extended regular expressions in the script')
-    parser.add_argument('-s', '--separate', action='store_true',
-                        help='consider files as separate rather than as a single, '
-                        'continuous long stream.')
-    parser.add_argument('--sandbox', action='store_true',
-                        help='operate in sandbox mode (disable e/r/w commands).')
-    # This is just done by default - here for compatibility only
-    parser.add_argument('-u', '--unbuffered', action='store_true',
-                        help='load minimal amounts of data from the input files and flush '
-                        'the output buffers more often')
-    parser.add_argument('--end', type=str, default='\n',
-                        help='end-of-line character for parsing search files (default: \\n); '
-                        'this does not affect file parsing for -f or --exclude-from')
-    parser.add_argument('-z', '--null-data', action='store_true',
-                        help='same as --end=\'\\0\'')
-    parser.add_argument('--version', action='store_true',
-                        help='output version information and exit')
-    parser.add_argument('--verbose', action='store_true', help='show verbose errors')
-    args = parser.parse_args(cliargs)
+        parser.add_argument('script', type=str, nargs='?', default=None,
+                            help='script, only if no other script defined below')
+        parser.add_argument('input_file', metavar='input-file', type=str, nargs='*', default=[],
+                            help='Input file(s) to parse')
 
-    if args.expression or args.file:
-        # The first positional is instead an input file
-        if args.script is not None:
-            args.input_file.insert(0, args.script)
-            args.script = None
-    elif args.script is None and not args.version:
-        parser.print_help()
-        sys.exit(1)
+        parser.add_argument('-n', '--quiet', '--silent', dest='quiet', action='store_true',
+                            help='suppress automatic printing of pattern space')
+        # No debug data
+        parser.add_argument('--debug', action='store_true', help='annotate program execution')
+        parser.add_argument('-e', '--expression', metavar='script', type=str, default=[], action='append',
+                            help='add the script to the commands to be executed')
+        parser.add_argument('-f', '--file', metavar='script-file', type=str, default=[], action='append',
+                            help='add the contents of script-file to the commands to be executed')
+        parser.add_argument('--follow-symlinks', action='store_true',
+                            help='follow symlinks when processing in place')
+        parser.add_argument('-i', '--in-place', metavar='SUFFIX', nargs='?', type=str, default=None,
+                            const=True,
+                            help='edit files in place (makes backup if SUFFIX supplied)')
+        parser.add_argument('-l', '--line-length', metavar='N', type=int, default=70,
+                            help='specify the desired line-wrap length for the `l\' command')
+        # This option currently has no effect
+        parser.add_argument('--posix', action='store_true', help='disable all extensions.')
+        parser.add_argument('-E', '-r', '--regexp-extended', action='store_true',
+                            help='use extended regular expressions in the script')
+        parser.add_argument('-s', '--separate', action='store_true',
+                            help='consider files as separate rather than as a single, '
+                            'continuous long stream.')
+        parser.add_argument('--sandbox', action='store_true',
+                            help='operate in sandbox mode (disable e/r/w commands).')
+        # This is just done by default - here for compatibility only
+        parser.add_argument('-u', '--unbuffered', action='store_true',
+                            help='load minimal amounts of data from the input files and flush '
+                            'the output buffers more often')
+        parser.add_argument('--end', type=str, default='\n',
+                            help='end-of-line character for parsing search files (default: \\n); '
+                            'this does not affect file parsing for -f or --exclude-from')
+        parser.add_argument('-z', '--null-data', action='store_true',
+                            help='same as --end=\'\\0\'')
+        parser.add_argument('--version', action='store_true',
+                            help='output version information and exit')
+        parser.add_argument('--verbose', action='store_true', help='show verbose errors')
+        args = parser.parse_args(cliargs)
 
-    return args
+        if args.expression or args.file:
+            # The first positional is instead an input file
+            if args.script is not None:
+                args.input_file.insert(0, args.script)
+                args.script = None
+        elif args.script is None and not args.version:
+            parser.print_help()
+            sys.exit(1)
 
-def _expand_cli_path(path:str) -> List[str]:
-    if IS_WINDOWS and '*' in path or '?' in path:
-        # Need to manually expand this out
-        expanded_paths = [f for f in glob.glob(path)]
-        if not expanded_paths:
-            print('No match for: {}'.format(path), file=sys.stderr)
-    else:
-        # *nix and *nix based systems do this from command line
-        expanded_paths = [path]
-    return expanded_paths
+        return args
 
-def _expand_cli_paths(paths:List[str]) -> List[str]:
-    return [y for x in paths for y in _expand_cli_path(x)]
+    @staticmethod
+    def _expand_cli_path(path:str) -> List[str]:
+        if IS_WINDOWS and '*' in path or '?' in path:
+            # Need to manually expand this out
+            expanded_paths = [f for f in glob.glob(path)]
+            if not expanded_paths:
+                print('No match for: {}'.format(path), file=sys.stderr)
+        else:
+            # *nix and *nix based systems do this from command line
+            expanded_paths = [path]
+        return expanded_paths
 
-def main(cliargs:List[str]) -> int:
-    ''' Command line main execution point '''
-    args = parse_args(cliargs)
-    if args.version:
-        print('{} {}'.format(PACKAGE_NAME, __version__))
-        return 0
+    @staticmethod
+    def _expand_cli_paths(paths:List[str]) -> List[str]:
+        return [y for x in paths for y in __class__._expand_cli_path(x)]
 
-    sed = Sed()
-
-    try:
+    def parse(self, sed:Sed) -> None:
+        ''' Parses command line arguments into Sed object '''
+        args = self.args
+        if args.version:
+            print('{} {}'.format(PACKAGE_NAME, __version__))
+            sys.exit(0)
         sed.suppress_pattern_print = args.quiet
         sed.extended_regex = args.regexp_extended
         sed.unambiguous_line_len = args.line_length
@@ -265,18 +268,27 @@ def main(cliargs:List[str]) -> int:
             sed.add_expression(args.script)
         for expression in args.expression:
             sed.add_expression(expression)
-        for file in _expand_cli_paths(args.file):
+        for file in __class__._expand_cli_paths(args.file):
             with open(file, 'r') as fp:
                 sed.add_expression(fp.read())
         if args.input_file:
-            sed.add_file(_expand_cli_paths(args.input_file))
+            sed.add_file(__class__._expand_cli_paths(args.input_file))
         if args.in_place is not None:
             sed.in_place = True
             if isinstance(args.in_place, str):
                 sed.in_place_backup_suffix = args.in_place
+
+def main(cliargs:List[str]) -> int:
+    ''' Command line main execution point '''
+
+    sed = Sed()
+    sed_arg_parser = SedArgParser(cliargs)
+
+    try:
+        sed_arg_parser.parse(sed)
         return sed.execute(sys.stdout.buffer)
     except Exception as ex:
-        if args.verbose:
+        if sed_arg_parser.args.verbose:
             raise ex
         else:
             print(f'{PACKAGE_NAME}: {ex}', file=sys.stderr)
